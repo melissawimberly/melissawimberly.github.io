6f46ce83ae6ae96fb5d59987402d324e68e8d571
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2014-11-09 17:41:03 -0500

    Adds my webpage html and css. (it also adds the mistake css file to stylesheets)

---
+++ stylesheets/webpage-stylesheet.css
@@ -0,0 +1,27 @@
+*{
+	background-color: blue;
+}
+
+td{
+border:1px solid blue;
+}
+
+
+.nav_bar{
+border: 1px solid teal;
+margin: 20px 10px 60px;
+}
+
+
+.background_img{
+position: absolute;
+margin-top: 200px;
+width: 50%;
+}
+
+
+.cover_img{
+position: relative;
+top: 200px;
+
+}
